a4299ea8ee86e18c62aa9ef4aa99ff92f4c737f8
Teemu Erkkola <teemu.erkkola@gofore.com>
2022-10-24 10:35:12 +0300

    Prompt user for unsaved changes

--- ckanext/markdown_editor/assets/js/ckeditor5-markdown.js
+++ ckanext/markdown_editor/assets/js/ckeditor5-markdown.js
@@ -4,8 +4,17 @@ ckan.module('ckeditor5-markdown', function (jQuery, _) {
     initialize: function () {
       ClassicEditor
         .create(this.el[0], {'language': this.options.language})
-        //.then( editor => console.log(editor) )
-        .catch( error => console.log(error) );
+      .then(editor => {
+        // Check if data is changed on unload to display browser prompt
+        const data = editor.getData();
+
+        addEventListener('beforeunload', (event) => {
+          if(editor.getData() !== data) {
+            return event.returnValue = true;
+          }
+        });
+      })
+      .catch( error => console.log(error) );
     },
   }
 });
