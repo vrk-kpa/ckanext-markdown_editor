03099bb6ea3eda15b734d62b9c4cab5bef4e38b7
Teemu Erkkola <teemu.erkkola@gofore.com>
2022-11-07 09:59:08 +0200

    Disable beforeunload notification when input element's form is submitted

--- ckanext/markdown_editor/assets/js/ckeditor5-markdown.js
+++ ckanext/markdown_editor/assets/js/ckeditor5-markdown.js
@@ -2,14 +2,20 @@ ckan.module('ckeditor5-markdown', function (jQuery, _) {
   return {
     options: {'language': ''},
     initialize: function () {
+      let el = this.el[0];
       ClassicEditor
-        .create(this.el[0], {'language': this.options.language})
+        .create(el, {'language': this.options.language})
       .then(editor => {
+        // Disable dirty check if form is being submitted
+        let submitted = false;
+        $(el).parents('form').submit((e) => {
+          submitted = true
+        });
+
         // Check if data is changed on unload to display browser prompt
         const data = editor.getData();
-
         addEventListener('beforeunload', (event) => {
-          if(editor.getData() !== data) {
+          if(!submitted && editor.getData() !== data) {
             return event.returnValue = true;
           }
         });
